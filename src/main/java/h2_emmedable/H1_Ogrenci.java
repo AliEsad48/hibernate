898785757f0a3ba16ebcce3b069b0aaff9f7e44b
Salih Bayram <alicamci2020@gmail.com>
2021-10-21 21:25:23 +0300

    V7

--- src/main/java/h2_emmedable/H1_Ogrenci.java
+++ src/main/java/h2_emmedable/H1_Ogrenci.java
@@ -1,7 +1,18 @@
 package h2_emmedable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ogrenciler")
 public class H1_Ogrenci {
 
+    @Id
+    private int ogrId;
 
+    private String ogrAd;
+
+    private int ogrNot;
 
 }
