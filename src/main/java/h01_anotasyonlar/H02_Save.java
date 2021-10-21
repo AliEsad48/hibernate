a534045c9685f16ddb0be5223cc34c20a1bd2911
Salih Bayram <alicamci2020@gmail.com>
2021-10-21 11:15:03 +0300

    V3

--- src/main/java/h01_anotasyonlar/H02_Save.java
+++ src/main/java/h01_anotasyonlar/H02_Save.java
@@ -1,6 +1,9 @@
 package h01_anotasyonlar;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class H02_Save {
@@ -10,10 +13,27 @@ public class H02_Save {
         //sql verilerinin olduğu cfg.xml clasına bağlandık, bu sayede sql e bağlandık
         //ve H01_Sehir classından işlem yapacağımızı belirttik
 
+        //işlem yapacağımız alanlar için (session) için parent alan oluşturduk
+        SessionFactory sf = con.buildSessionFactory();
+        //parent alandan kendime küçük alanlar açtım
+        Session s = sf.openSession();
 
+        // Acilan session icerisinde islemlere baslayabilmek icin Transaction aciyoruz.
+        /*Transactionlar ile bir işlem yarıda kaldıysa veya
+         * tam olarak tamamlanadıysa tüm adımlar başa alınır
+         * veri ve işlem güvenliği için önemlidir.Kısacası ya hep
+         * ya hiç prensibine göre çalışır
+         */
+
+        Transaction tx = s.beginTransaction();
 
         H01_Sehir sehir1 = new H01_Sehir(34, "İstanbul");
         H01_Sehir sehir2 = new H01_Sehir(35, "İzmir");
+
+        s.save(sehir1);
+        s.save(sehir2);
+
+        tx.commit();
 
 
     }
