ddaf4873a1e22fa43bb92a1fce00952cd4fe10c4
Salih Bayram <alicamci2020@gmail.com>
2021-10-21 20:56:15 +0300

    V4

--- src/main/java/h01_anotasyonlar/H02_Save.java
+++ src/main/java/h01_anotasyonlar/H02_Save.java
@@ -1,6 +1,5 @@
 package h01_anotasyonlar;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -8,33 +7,45 @@ import org.hibernate.cfg.Configuration;
 
 public class H02_Save {
     public static void main(String[] args) {
+        //İMPORTLARI HİBERNATE OLANDAN yap
+        // Veritabani baglanti ayarlarini Hibernate'e gostermeliyiz.hibernate den import et
+//      Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H1_Sehir.class);
+//                                //üstteki url lere göre hibernate i (şifre isim vs) ayarla demek//üstteki class ı göz önüne al
+//
+//      // con objesinden bir oturum grubu oluşturduk.session açıyoruz işimiz bitince kapatıyoruz en altta
+//      SessionFactory sf = con.buildSessionFactory();//tüm session ların anası, parent ı, bunun altına bir sürü session oluşturabiliriz,transaction lada açarız
 
-        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H01_Sehir.class);
-        //sql verilerinin olduğu cfg.xml clasına bağlandık, bu sayede sql e bağlandık
-        //ve H01_Sehir classından işlem yapacağımızı belirttik
 
-        //işlem yapacağımız alanlar için (session) için parent alan oluşturduk
-        SessionFactory sf = con.buildSessionFactory();
-        //parent alandan kendime küçük alanlar açtım
-        Session s = sf.openSession();
+        SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(H01_Sehir.class).buildSessionFactory();
 
+        // Oturum grubundan bir oturumu başlattık.
+        Session session = sf.openSession();
         // Acilan session icerisinde islemlere baslayabilmek icin Transaction aciyoruz.
-        /*Transactionlar ile bir işlem yarıda kaldıysa veya
+        /*Transactionlar ile bir işlem yarıda kaldıysa  veya
          * tam olarak tamamlanadıysa tüm adımlar başa alınır
          * veri ve işlem güvenliği için önemlidir.Kısacası ya hep
          * ya hiç prensibine göre çalışır
          */
+        Transaction tx = session.beginTransaction();
 
-        Transaction tx = s.beginTransaction();
 
-        H01_Sehir sehir1 = new H01_Sehir(34, "İstanbul");
-        H01_Sehir sehir2 = new H01_Sehir(35, "İzmir");
+        // ilk burayı yaz,sonra üstleri
+        // Tabloya eklenecek verileri (record, kayit) olusturmamız gerekiyor.her run ettiğimizde id ler değiştirilmeli çünkü önceki kalıcı kayıtlı
+        H01_Sehir sehir1 = new H01_Sehir(34, "Istanbul", 10000000);
 
-        s.save(sehir1);
-        s.save(sehir2);
 
+//      H1_Sehir sehir2 = new H1_Sehir(38,"Izmir", 2500000);
+        // Veritabanina kayitlarin eklenmesi.(kalıcı persistion) bu=> yaz demek, commit=>  yazmayı kesinleştiriyor
+        session.save(sehir1);
+
+        //session.save(new H1_Sehir(38, "Istanbul", 10000000));//insert into gibi
+        session.save(new H01_Sehir(35, "Izmir", 2500000));
+
+        // İslemlerin veritabanina aktarilmasi(yazmakta fayda var)
         tx.commit();
-
-
+        // Oturumlarin kapatilmasi
+        sf.close(); //bunu kapatınca parent kapamış olur, session açılamaz
+        session.close();//kapattıktan sonra işlem yapılmaz, yapmak istersek tekrar session lar açmalıyız
     }
 }
---
+++ src/main/java/h01_anotasyonlar/H03_Fetch.java
@@ -0,0 +1,22 @@
+package h01_anotasyonlar;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class H03_Fetch {
+
+    public static void main(String[] args) {
+
+        SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H01_Sehir.class).buildSessionFactory();
+
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        System.out.println(session.get(H01_Sehir.class, 34));
+
+    }
+
+}
